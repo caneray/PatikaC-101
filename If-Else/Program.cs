77b3dd1b63466dd0ba0b977e29d2b7d904ef64fb
Caner AY <caneray34@hotmail.com>
2024-06-18 13:56:57 +0300

    Projects loaded

---
+++ If-Else/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time<11)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Geceler!");
+
+
+            //string sonuc = time < 18 ? "İyi Günler" : "İyi Geceler";
+
+            string sonuc = time >= 6 && time < 11 ? "Günaydın" : time < 18 ? "İyi Günler!" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n + "x" + i + "=" + n * i);
+            }
+        }
+    }
+}
