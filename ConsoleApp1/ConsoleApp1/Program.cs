4d79b7f1e83ca86f2622f79f440d4ab5e0f988af
Caner AY <caneray34@hotmail.com>
2024-06-18 12:35:48 +0300

    Projects loaded

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Security;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte b = 5;         // 1 byte
+            sbyte a = 5;        // 1 byte
+            short s = 5;        // 2 byte  
+            ushort us = 5;      // 2 byte (0, 65365)
+
+            Int16 i16 = 2;          // 2 byte
+            int i = 2;              // 4 byte
+            Int32 i32 = 2;          // 4 byte
+            Int64 i64 = 2;          // 8 byte
+
+            uint ui = 2;            // 4 byte
+            long l = 2;             // 8 byte
+            ulong ul = 2;           // 8 byte
+
+            // Reel sayılar
+            float f = 2;            // 4 byte
+            double d = 2;           // 8 byte
+            decimal c = 2;          // 16 byte
+
+            char ch = '2';          // 2 byte
+            string str = "Caner";   // sınırsız
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+
+            object o1 = "x"; //string
+            object o2 = 'c'; //char
+            object o3 = 3;
+            object o4 = 4.3;
+
+            // String ifadeler;
+
+            string str1 = string.Empty; //boş bir string
+            str1 = "Caner Ay";
+
+            string ad = "Caner ";
+            string soyad = "Ay";
+
+            string tamIsim = ad + " " + soyad;
+
+            // Integer ifadeler;
+
+            int int1 = 16;
+            int int2 = 25;
+
+            int carpim = int1 * int2;
+
+            // Boolean
+
+            bool bool1 = 10 < 2; //false
+            bool bool2 = 10 > 2; //true
+
+
+            // Değişken Dönüşümleri;
+
+            string str20 = "20";
+            int int20 = 24;
+
+            string yeniDeger = str20 + int20.ToString();
+
+            Console.WriteLine(yeniDeger); // Çıktısı 2024
+
+
+            // Integer Dönüşümler;
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine(int21); // Çıktısı 44
+
+            int int22 = int20 + int.Parse(str20);
+            Console.WriteLine(int22); // Çıktısı 44
+
+
+            // DateTime
+
+            string datetime = DateTime.Now.ToString("dd.mm.yyyy");
+            Console.WriteLine(datetime);
+
+        }
+    }
+}
---
+++ TipDonusumleri/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace TipDonusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion (Bilinçsiz Dönüşüm) => Düşük kapasiteliden yüksek kapasiteli
+
+            Console.WriteLine("******* Implicit Conversion *******");
+            Console.WriteLine(" ");
+            byte a = 5;
+            sbyte b = 30;
+            short c = 25;
+
+            int d = a + b + c;
+            Console.WriteLine("d: " + d);
+
+            long h = d;
+            Console.WriteLine("h: " + h);
+
+            float i = h;
+            Console.WriteLine("i: " + i);
+
+            string e = "Caner";
+            char f = 'a';
+            char ay = 'y';
+            object g = e + f + ay + d;
+            Console.WriteLine("g: " + g);
+
+
+
+            // Explicit Conversion (Bilinçli Dönüşüm) => C# ın kendi kendine yapamadığı dönüşümler. Convert, Parse ve ToString kullanarak dönüşümler yapılır.
+            Console.WriteLine(" ");
+            Console.WriteLine("******* Explicit Conversion *******");
+            Console.WriteLine(" ");
+
+
+            int x = 4;
+            byte y = (byte)x;
+            Console.WriteLine("y: " + y);
+
+            int z = 100;
+            byte t = (byte)z;
+            Console.WriteLine("t: " + t);
+            
+            float w = 10.3f; //veri kaybı , çevrimlerde veri kaybı olabilir göz önüne alınabilir.
+            byte v = (byte)w;
+            Console.WriteLine("v: " + v);
+
+            // ToString Methodu
+            Console.WriteLine(" ");
+            Console.WriteLine("******* ToString Methodu *******");
+            Console.WriteLine(" ");
+
+            int xx = 6;
+            string yy = xx.ToString();
+            Console.WriteLine("yy: " + yy);
+
+            string zz = 12.5f.ToString();
+            Console.WriteLine("zz: " + zz);
+
+            // Convert Methodu
+            Console.WriteLine(" ");
+            Console.WriteLine("******* Convert Methodu *******");
+            Console.WriteLine(" ");
+
+            string s1 = "10", s2 = "20";
+            int sayi1, sayi2;
+            int Toplam;
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+            Toplam = sayi1 + sayi2;
+            Console.WriteLine("Toplam: " + Toplam);
+
+            
+            // Parse Methodu
+            Console.WriteLine(" ");
+            Console.WriteLine("******* Parse Methodu *******");
+            Console.WriteLine(" ");
+
+            ParseMethod();
+
+
+        }
+
+        public static void ParseMethod()
+        {
+            string metin1 = "10";
+            string metin2 = "10,25";
+            int rakam1;
+            double double1;
+
+            rakam1 = Int32.Parse(metin1);
+            double1 = Double.Parse(metin2);
+            Console.WriteLine("Rakam1 : " + rakam1);
+            Console.WriteLine("Double1 : " + double1);
+            
+
+
+        }
+    }
+
+    
+}
