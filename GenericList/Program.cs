e41bfdd8a98e80f72e79c57d09ae464fb10e64f0
Caner AY <caneray34@hotmail.com>
2024-06-27 14:20:48 +0300

    Project loaded

---
+++ GenericList/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // syntax yapısı List<T> class
+            // System.Collections.Generic
+            // T -> Object türündedir. Liste içerisindeki nesnenin tipini gösterir.
+
+            List<int> sayiListesi = new List<int>();
+
+            sayiListesi.Add(23);
+            sayiListesi.Add(10);
+            sayiListesi.Add(4);
+            sayiListesi.Add(5);
+            sayiListesi.Add(92);
+            sayiListesi.Add(34);
+
+            List<string> renkListesi = new List<string>();
+
+            renkListesi.Add("Kırmızı");
+            renkListesi.Add("Mavi");
+            renkListesi.Add("Turuncu");
+            renkListesi.Add("Sarı");
+            renkListesi.Add("Yeşil");
+
+            // Count
+            Console.WriteLine(renkListesi.Count); //Liste içerisindeki eleman sayısı
+            Console.WriteLine(sayiListesi.Count); //Liste içerisindeki eleman sayısı
+
+            // Foreach ve List.Foreach ile Elemanları yazdırma
+            foreach (var sayi in sayiListesi)
+                Console.WriteLine(sayi);
+            foreach (var renk in renkListesi)
+                Console.WriteLine(renk);
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            // Listeden Eleman Çıkartma
+            sayiListesi.Remove(4); // 4 ü çıkar
+            renkListesi.Remove("Yeşil");
+
+            sayiListesi.RemoveAt(0); // 0. indexteki elemanı çıkar
+            renkListesi.RemoveAt(1); // 1. indexteki elemanı çıkar
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            // Liste İçerisinde Arama
+            if (sayiListesi.Contains(10))
+                Console.WriteLine("10 Liste içerisinde bulundu!");
+
+            // Elemanı vererek indexe erişim
+            Console.WriteLine(renkListesi.BinarySearch("Kırmızı"));
+
+            // Diziyi List'e çevirme
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş" };
+            List<string> hayvanListesi = new List<string>(hayvanlar);
+
+            // Listeyi Temizleme
+            hayvanListesi.Clear();
+
+            // List içerisinde nesne tutmak
+            List<Kullanicilar> kullaniciListesi = new List<Kullanicilar>();
+
+            Kullanicilar kullanici1 = new Kullanicilar();
+            kullanici1.Isim = "Caner";
+            kullanici1.Soyisim = "Ay";
+            kullanici1.Yas = 24;
+
+            Kullanicilar kullanici2 = new Kullanicilar();
+            kullanici2.Isim = "Çağla";
+            kullanici2.Soyisim = "Şılak";
+            kullanici2.Yas = 23;
+
+            kullaniciListesi.Add(kullanici1);
+            kullaniciListesi.Add(kullanici2);
+
+            List<Kullanicilar> yeniListe = new List<Kullanicilar>();
+
+            yeniListe.Add(new Kullanicilar()
+            {
+                Isim = "Buse",
+                Soyisim = "Ay",
+                Yas = 18
+            });
+
+            foreach (var kullanici in kullaniciListesi)
+            {
+                Console.WriteLine("Kullanıcı Adı: " + kullanici.Isim);
+                Console.WriteLine("Kullanıcı Soyadı: " + kullanici.Soyisim);
+                Console.WriteLine("Yaşı: " + kullanici.Yas);
+            }
+            yeniListe.Clear();
+
+
+        }
+    }
+
+    public class Kullanicilar
+    {
+        private string isim;
+        private string soyisim;
+        private int yas;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int Yas { get => yas; set => yas = value; }
+
+    }
+}
---
+++ HazirDateTimeMathMetotlar/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace HazirDateTimeMathMetotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek); // Haftanın gününü string halde getirir.
+            Console.WriteLine(DateTime.Now.DayOfYear); // Yılın kaçıncı günündeyim?
+
+            Console.WriteLine(DateTime.Now.ToLongDateString()); // UI da kullanılabilir
+            Console.WriteLine(DateTime.Now.ToShortDateString()); // Daha çok kullanılır.
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(30));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(10));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+            // DateTimee Format
+
+            Console.WriteLine(DateTime.Now.ToString("dd")); //günün int karşılığı
+            Console.WriteLine(DateTime.Now.ToString("ddd")); //günün string karşılığı(kısa) - Sat
+            Console.WriteLine(DateTime.Now.ToString("dddd")); //günün string karşılığı(uzun) - Saturday
+
+            Console.WriteLine(DateTime.Now.ToString("MM")); // ayın int karşılığı
+            Console.WriteLine(DateTime.Now.ToString("MMM")); // ayın string karşılığı (kısa) - Apr
+            Console.WriteLine(DateTime.Now.ToString("MMMM")); // ayın string karşılığı (uzun) - April
+
+            Console.WriteLine(DateTime.Now.ToString("yy")); // yılın kısaltılmış hali - 24
+            Console.WriteLine(DateTime.Now.ToString("yyyy")); // yılın uzatılmış hali - 2024
+
+            Console.WriteLine("***************** MATH KÜTÜPHANESİ *****************");
+            
+            Console.WriteLine(Math.Abs(-25)); // Mutlak değer alır. (25)
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+            
+            Console.WriteLine(Math.Ceiling(22.3)); // 22.3 den büyük en küçük tamsayı (23)
+            Console.WriteLine(Math.Round(22.3)); // Hangisine daha yakınsa onu verir. 22ye daha yakın 22 çıktısını verir.
+            Console.WriteLine(Math.Floor(22.7)); // 22.7 den küçük en büyük tamsayı (22)
+            Console.WriteLine(Math.Max(2,6)); // 2 sayının max olanı getirir.
+            Console.WriteLine(Math.Min(2,6)); // 2 sayının min olanı getirir.
+            
+            Console.WriteLine(Math.Pow(3,4)); // üst alma. 81 çıktısı verir
+            Console.WriteLine(Math.Sqrt(9)); // Karekök alır. 3 verir
+            Console.WriteLine(Math.Log(9)); // Log alır. 9 un e tabanındaki log karşılığı
+            Console.WriteLine(Math.Log10(100)); // Log alır. 100 un 10 tabanındaki log karşılığı
+            Console.WriteLine(Math.Exp(3)); // e üzeri 3 ü verir.
+
+
+
+        }
+    }
+}
