19cfcb10022d461282dccf03d8db2659c65ed37c
Caner AY <caneray34@hotmail.com>
2024-07-03 15:22:34 +0300

    Project loaded.

---
+++ StaticSinifveUyeler/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace StaticSinifveUyeler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı = {0}",Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Caner","Ay","Yazılım");
+            Console.WriteLine("Çalışan Sayısı = {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan1 = new Calisan("Deniz", "Arda", "Yazılım");
+            Calisan calisan2 = new Calisan("Çağla", "Şılak", "Yazılım");
+            Console.WriteLine("Çalışan Sayısı = {0}", Calisan.CalisanSayisi);
+
+
+            //Islemler islemler = new Islemler(); // Erişilmez
+
+            //Islemler.Topla(100, 200); //statik sınıflara SinifAdi. şeklinde erişilir.
+            Console.WriteLine("Toplama İşlemi Sonucu = {0}", Islemler.Topla(100, 200));
+            Console.WriteLine("Çıkarma İşlemi Sonucu = {0}", Islemler.Cikar(400, 200));
+        }
+    }
+
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+        public Calisan(string ısim, string soyisim, string departman)
+        {
+            this.Isim = ısim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+    }
+
+    static class Islemler //Statik sınıfların içerisine static harici herhangi bir sınıf tanımlayamayız.
+    {
+        public static long Topla(int sayi1, int sayi2) 
+        {
+            return sayi1 + sayi2;
+        }
+        public static long Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
---
+++ StructYapilar/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace StructYapilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+            Console.WriteLine("Class Alan Hesabı : {0}", dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct;
+            //Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct();
+            dikdortgen_Struct.KisaKenar = 3;
+            dikdortgen_Struct.UzunKenar = 4;
+            Console.WriteLine("Struct Alan Hesabı : {0}", dikdortgen_Struct.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_Struct1 = new Dikdortgen_Struct(3,4);
+            Console.WriteLine("Struct Alan Hesabı : {0}", dikdortgen_Struct1.AlanHesapla());
+
+
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen()
+        {
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        /* Parametresi olmayan constructor structlarda tanımlanamaz!
+        public Dikdortgen_Struct()
+        {
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }  */
+
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            this.KisaKenar = kisaKenar;
+            this.UzunKenar = uzunKenar;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+
+    }
+}
+
+/* NOT
+ 16 bayta kadar verileri saklamak için struct
+ 16 bayttan yüksek olan verileri class ile tutulmaswı daha sağlıklıdır.
+*/
